cd653dfcff86a5b068ddc7bab99e6264a8b7d044
wanglei <wanglei@88gongxiang.com>
2018-11-27 10:27:10 +0800

    commit

--- src/com/test/pro/arrow/function/Operator.java
+++ src/com/test/pro/arrow/function/Operator.java
@@ -3,7 +3,7 @@ package com.test.pro.arrow.function;
 public class Operator {
 
 	public static void main(String[] args) {
-		AddOperator<Long,Long> addOperator = (x, y)->x +y;
+		AddOperator<Long,Long> addOperator = (x, y)-> x + y;
 		System.err.println("Custom function listener res = " + addOperator.add(34L, 65L));
 	}
 
