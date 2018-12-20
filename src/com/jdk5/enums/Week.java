3b716f7af3e263cb270d360094230b1880d280e7
wanglei <wanglei@88gongxiang.com>
2018-12-20 16:45:14 +0800

    commit 重构~

--- src/com/jdk5/enums/Week.java
+++ src/com/jdk5/enums/Week.java
@@ -64,17 +64,9 @@ public enum Week {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getDesc() {
         return desc;
     }
 
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-    
     public abstract void print();
 }
---
+++ src/com/refactoring/Rental.java
@@ -0,0 +1,21 @@
+package com.refactoring;
+
+public class Rental {
+	private Movie _movie;
+	private int _daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		super();
+		this._movie = movie;
+		this._daysRented = daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+}
